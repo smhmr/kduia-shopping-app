54bdef8c84eae6ea594e0731efb63421f0c526e9
smhmr <116069860+smhmr@users.noreply.github.com>
2023-03-13 10:56:26 +0500

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,40 +1,31 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { FaTimesCircle } from 'react-icons/fa';
+
+const ExpenseItem = (props) => {
+    const { dispatch, Location} = useContext(AppContext);
+
+    const handleDeleteItem = () => {
+        const item = {
+            name: props.name,
+        };
+
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item,
+        });
+    };
 
 
-import { AppProvider } from './context/AppContext';
-import CartValue from './components/CartValue';
-import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
-import Location from './components/Location';
-
-const App = () => {
     return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Shopping App</h1>
-                <div className='row mt-3'> 
-                    <div className='col-sm'>
-                        <CartValue />
-                    </div>
-                    <div className='col-sm'>
-                        <Location />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Shopping Cart</h3>
-                <div className='row '>
-                    <div className='col-sm'>
-                        <ExpenseList />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Add Items</h3>
-                <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <ItemSelected/>
-                    </div>
-                </div>
-            </div>
-        </AppProvider>
+        <tr>
+        <td>{props.name}</td>
+        <td>{props.quantity}</td>
+        <td>{Location}{parseInt(props.unitprice)}</td>
+        <td>{Location}{parseInt(props.quantity)*parseInt(props.unitprice)}</td>
+        <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td>
+        </tr>
     );
 };
-export default App;
+
+export default ExpenseItem;
